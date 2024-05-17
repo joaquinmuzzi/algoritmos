6bdfe629b4134aab410a5f0fc69a3309bf882cec
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-05-16 23:06:33 -0300

    add 7d.c

---
+++ practica2/7d.c
@@ -0,0 +1,15 @@
+// Una función que devuelva un entero aleatorio en el intervalo [0, 𝑛)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int ran_num(int asintota){
+    return rand() % asintota;
+}
+
+int main(int argc, char const *argv[]){
+    srand(time(NULL));
+    printf("%i\n", ran_num(atoi(argv[1])));
+    return 0;
+}
