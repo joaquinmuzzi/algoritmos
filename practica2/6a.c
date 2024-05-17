1aeddce0aab9bbc5ae3534161c29d14d5ce3ab5a
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-05-16 21:05:03 -0300

    add 6a

---
+++ practica2/6a.c
@@ -0,0 +1,14 @@
+// Una función que decida si un número entero 𝑏 es divisor de otro entero 𝑎.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char* is_divisible(int a, int b){
+    return a % b == 0 ? "Es divisible" : "No es divisible";
+}
+
+int main(int argc, char const *argv[]){
+    printf("%s\n", is_divisible(atoi(argv[1]), atoi(argv[2])));
+    return 0;
+}
+
