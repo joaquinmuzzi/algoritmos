746ad5b10c026f908a2f000eae6b9ad561c47568
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-05-16 23:28:29 -0300

    add 7f

---
+++ practica2/7f.c
@@ -0,0 +1,15 @@
+// Una función que reciba dos enteros 𝑎 y 𝑏 y devuelva un entero aleatorio en el intervalo [𝑎, 𝑏].
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int ran_num(int min, int max){
+    return rand() % (max - min + 1) + min;
+}
+
+int main(int argc, char const *argv[]){
+    srand(time(NULL));
+    printf("%i\n", ran_num(atoi(argv[1]), atoi(argv[2])));
+    return 0;
+}
