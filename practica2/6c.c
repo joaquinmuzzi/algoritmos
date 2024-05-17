a61eb78fa163d53075de88d5650588003c051e3d
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-05-16 21:17:01 -0300

    add 6c

---
+++ practica2/6c.c
@@ -0,0 +1,13 @@
+// Una función que acepte un año y decida si es bisiesto o no.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char* es_bisiesto(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? "Es bisiesto" : "No es bisiesto";
+}
+
+int main(int argc, char const *argv[]){
+    printf("%s\n", es_bisiesto(atoi(argv[1])));
+    return 0;
+}
