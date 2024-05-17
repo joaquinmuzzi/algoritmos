21516082799c33c7965a3a78e4a4d92c2263d6ce
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-05-17 13:49:31 -0300

    fix area

--- area/area.c
+++ area/area.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <math.h>
 
-double anillo(int D, int d){
-  double area_ext = M_PI * (d * 0.5) * (d * 0.5);
-  double area_int = M_PI * (D * 0.5) * (D * 0.5);
+double anillo(double D, double d){
+  double area_ext = 3.1415 * (d * 0.5) * (d * 0.5);
+  double area_int = 3.1415 * (D * 0.5) * (D * 0.5);
   return area_ext - area_int;
 }
 
 double area(char fig, int a, int b){
   return fig == 't' ? a * b / 2 : ( fig == 'r' ? a * b :
-        (fig == 'a' ? anillo(a, b) : ' '));
+        (fig == 'a' ? anillo(a, b) : 0));
 
 }
 
 int main(int argc, char *argv[]){
-  printf("%f\n", area(argv[1][0], atoi(argv[1]), atoi(argv[2])));
+  printf("%f\n", area(argv[1][0], atoi(argv[2]), atoi(argv[3])));
   return 0;
 }
