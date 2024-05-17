7e1cdb93a9ce7b8340f36371a53b2f898f9c3833
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-05-17 08:44:04 -0300

    fix 5e

--- practica2/5e.c
+++ practica2/5e.c
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-double cuadraticFunc(double a, double b, double c){
-    double x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
-    double x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
-    return "Primera raiz: %f\n Segunda raiz: %f\n", x1, x2;
-}
-=======
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -22,5 +15,3 @@ int main(int argc, char *argv[]){
     raices(atof(argv[1]), atof(argv[2]), atof(argv[3]));
     return 0;
 }
-
->>>>>>> 80b037c461e012b520220f378f2e852fd5cb8f42
