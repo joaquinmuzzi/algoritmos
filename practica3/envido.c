a4753c97d2012ec971b4b86ae6428c98ad41f409
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-05-23 20:49:57 -0300

    modifica envido

--- practica3/envido.c
+++ practica3/envido.c
@@ -1,1 +1,10 @@
-int envido(char p1, int v1, char p2, int v2, char p3, int v3)
+#include "stdlib.h"
+#include "stdio.h"
+
+int envido(char p1, int v1, char p2, int v2, char p3, int v3){
+
+    return resultado;
+}
+
+// b 7 o 10 c 8 = 7 
+// b 7 o 10 b 8 = 15 
