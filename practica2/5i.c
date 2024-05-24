009240856e39d9ba73cb425935c0e5a114c138e6
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-05-24 10:12:17 -0300

    modifica 5i

--- practica2/5i.c
+++ practica2/5i.c
@@ -7,7 +7,7 @@ void prod_vect(double x1, double x2, double x3, double y1, double y2, double y3)
     double i = x2 * y3 - x3 * y2;
     double j = -(x1 * y3 - y1 * x3);
     double k = x1 * y2 - x2 * y1;
-    printf("El resultado es: %f %f %f", i, j, k);
+    printf("El resultado es: %.2f %.2f %.2f", i, j, k);
 }
 
 int main(int argc, char *argv[]){
