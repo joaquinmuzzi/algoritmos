58cb144ef3594e9a675718bb277f936b0edb5ccd
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-05-23 21:12:14 -0300

    agrega envido

--- practica3/envido.c
+++ practica3/envido.c
@@ -2,8 +2,32 @@
 #include "stdio.h"
 
 int envido(char p1, int v1, char p2, int v2, char p3, int v3){
+    int resultado = 0;
+    if(p1 != p2 && p2 != p3 && p1 != p3){
+        resultado += v1 > v2 ? v2 : (v1 > v3 ? v3 : v1);
+        return resultado;
+    }
+    
+    v1 = v1 >= 10 ? 0 : v1;
+    v2 = v2 >= 10 ? 0 : v2;
+    v3 = v3 >= 10 ? 0 : v3;
 
+    if(p1 == p2){
+        resultado += v1 + v2;
+
+    }else if(p2 == p3){
+        resultado += v2 + v3;
+
+    }else if(p1 == p3){
+        resultado += v1 + v3;
+
+    }
     return resultado;
+}
+
+int main(int argc, char const *argv[]){
+    printf("%i\n", envido((argv[1][0]), atoi(argv[2]), (argv[3][0]), atoi(argv[4]), (argv[5][0]), atoi(argv[6])));
+    return 0;
 }
 
 // b 7 o 10 c 8 = 7 
