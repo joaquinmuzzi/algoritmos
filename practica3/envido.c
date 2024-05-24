3931320b12ff5161a10029fdd6b31712ee567997
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-05-23 21:40:56 -0300

    modifica envido

--- practica3/envido.c
+++ practica3/envido.c
@@ -5,16 +5,17 @@ int envido(char p1, int v1, char p2, int v2, char p3, int v3){
     // declaro la variable que va a tomar el valor del envido
     int resultado = 0;
 
+    // si algun valor es mayor o igual a 10 lo toma como 0
+    v1 = v1 >= 10 ? 0 : v1;
+    v2 = v2 >= 10 ? 0 : v2;
+    v3 = v3 >= 10 ? 0 : v3;
+    
     // si ningun palo es igual imprime el MAYOR valor
     if(p1 != p2 && p2 != p3 && p1 != p3){
         resultado += v1 < v2 ? v2 : (v1 < v3 ? v3 : v1);
         return resultado;
     }
 
-    // si algun valor es mayor o igual a 10 lo toma como 0
-    v1 = v1 >= 10 ? 0 : v1;
-    v2 = v2 >= 10 ? 0 : v2;
-    v3 = v3 >= 10 ? 0 : v3;
 
     // si dos palos son iguales suma sus valores
     if(p1 == p2){
