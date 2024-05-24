664dd9fb3adfe751b73bff1f82e63f95a3fbdd50
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-05-24 09:05:06 -0300

    añade maximo

---
+++ practica3/maximo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int maximo(int a, int b) { return a > b ? a : b; }
+
+int main(int argc, char const *argv[]) {
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int c = atoi(argv[3]);
+    printf("El maximo de %d %d %d es %d\n",a,b,c,maximo(c,maximo(a,b)));
+    return 0;
+}
