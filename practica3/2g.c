a4c3ee74de9a76ceefdc8d9eafe0445919f39afc
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-05-30 11:18:58 -0300

    modifica 2g

--- practica3/2g.c
+++ practica3/2g.c
@@ -1,11 +1,12 @@
 // Un programa suma los primeros 𝑛 números naturales. Al final imprime el valor de la suma.
+// Números triangulares
 
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[]){
   int limite = atoi(argv[1]);
-  int suma = 0;
-  for(int i = 0; i <= limite; i++){
+  int suma = 1;
+  for(int i = 1; i <= limite; i++){
     suma += i;
   }
   printf("La suma es: %i\n", suma);
