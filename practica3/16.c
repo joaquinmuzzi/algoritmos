e52cd628d3a77ea05c106262eef91ade96cbb21c
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-06-14 10:03:25 +0000

    modifica 16

--- practica3/16.c
+++ practica3/16.c
@@ -4,15 +4,22 @@
 int main(int argc, char *argv[]){
   int n = atoi(argv[1]);
 
+  for (int i = 1; i <= n; i++) {
+    printf("%i ", i);
+  }
+  printf("\n");
   for(int i = 1; i <= n; i++){
     for(int j = 1; j <= n; j++){
       if(i == 1){
-        printf("%i ", j);
-      }else{
+        printf("* ");
+      }else if(i == 2){
+        printf("* ");
+      }
+      else{
         printf(j % i == 0 || i % j == 0 ? "* " : "  ");
       }
     }
-    printf("\n");
+    printf("%i\n", i);
   }
 
 }
