fece761e5c57586b547f01ba317782a42774dc45
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-06-17 18:40:17 -0300

    agrega 31 - practica 3

--- practica3/31.c
+++ practica3/31.c
@@ -1,27 +1,22 @@
-//The prime factors of 13195 are 5, 7, 13 and 29.
-//What is the largest prime factor of the number 600851475143
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
 bool is_prime(int n){
     for (int i = 2; i < n; i++){
         if (n % i == 0) return false;
     }
     return true;
 }
-
 int main(int argc, char* argv[]) {
   int num = atoi(argv[1]);
     // bucle que se ejecuta mientras i sea menor a num 
     for (int i = 2; i <= num; i++){
-      // si es primo, es un divisor de num y tambien es menor a num lo imprime
-      if (is_prime(i)) {
-        while(num % i == 0){
+      // si es primo y es un divisor de num lo imprime
+        while(is_prime(i) && num % i == 0){
           printf("%i\n", i);
+          // divide num por el factor actual
           num /= i;
         }
-      }
     }
   return 0;
 }
