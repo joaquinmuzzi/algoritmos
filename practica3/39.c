451f14b9f38df28024e05da767aa200f5935c275
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-06-18 20:55:36 -0300

    modifica 39

--- practica3/39.c
+++ practica3/39.c
@@ -3,13 +3,23 @@ si los números son capicúas, es decir que se leen igual de izquierda a derecha
 que de derecha a izquierda. Dar el diagrama de flujo. */
 #include <stdio.h>
 #include <stdlib.h>
+char* es_capicua(int num){
+  int num_original = num;
+  int num_invertido = 0;
+  while (num != 0) {
+    num_invertido = (num_invertido * 10) + num % 10;
+    num /= 10;
+  }
+  return num_invertido == num_original ? "es capicula" : "no es capicua";
+}
 int main(int argc, char *argv[]) {
   int a = atoi(argv[1]);
+  int b = atoi(argv[2]);
   int a_invertido = 0;
-  while (a != 0) {
-    a_invertido = (a_invertido * 10) + a % 10;;
-    a /= 10;
+  int b_invertido = 0;
+  for (int i = 0; i < 2; i++) {
+    int numero_actual = i ? a : b;    
+    printf("%i %s\n", numero_actual, es_capicua(numero_actual));
   }
-  printf("%s\n", a_invertido == atoi(argv[1]) ? "es capicua" : "no es capicua");
   return 0;
 }
