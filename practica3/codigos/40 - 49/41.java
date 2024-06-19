4dff0d721236f68e8fb5de18ced296ee034908f6
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-06-19 00:41:15 -0300

    modifica 41

--- practica3/codigos/40 - 49/41.java
+++ practica3/codigos/40 - 49/41.java
@@ -4,9 +4,11 @@ line(). */
 size(400, 400);
 int centroHorizontal = width/2;
 int centroVertical = height/2;
-for(int i = 0; i < width/2; i += 10){
+for(int i = 5; i < width/2; i += 10){
   line(centroHorizontal-i,centroVertical-i,centroHorizontal+i,centroVertical-i);
   line(centroHorizontal+i,centroVertical-i,centroHorizontal+i,centroVertical+i);
-  line(centroHorizontal+i,centroVertical+i,centroHorizontal-i,centroVertical+i);
-  line(centroHorizontal-i,centroVertical+i,centroHorizontal-i,centroVertical-i);
+}
+for(int j = 10; j < width/2; j += 10){
+  line(centroHorizontal+j-5,centroVertical+j-5,centroHorizontal-j-5,centroVertical+j-5);
+  line(centroHorizontal-j-5,centroVertical+j-5,centroHorizontal-j-5,centroVertical-j-5);
 }
