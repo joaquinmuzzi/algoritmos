0992c42b1ab26784cb80f1ca066c03a74f60e678
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-06-18 21:09:49 -0300

    hace legible el 39 -_-

--- practica3/39.c
+++ practica3/39.c
@@ -6,19 +6,20 @@ que de derecha a izquierda. Dar el diagrama de flujo. */
 char* es_capicua(int num){
   int num_original = num;
   int num_invertido = 0;
+  int digito;
   while (num != 0) {
-    num_invertido = (num_invertido * 10) + num % 10;
-    num /= 10;
+    digito = num % 10; // toma el ultimo digito de num
+    num_invertido = (num_invertido * 10) + digito; // recrea el numero invertido
+    num /= 10; // "elimina" el ultimo digito de num
   }
+  //si el numero invertido es igual al original entonces es capicua
   return num_invertido == num_original ? "es capicula" : "no es capicua";
 }
 int main(int argc, char *argv[]) {
   int a = atoi(argv[1]);
   int b = atoi(argv[2]);
-  int a_invertido = 0;
-  int b_invertido = 0;
-  for (int i = 0; i < 2; i++) {
-    int numero_actual = i ? a : b;    
+  for (int i = 0; i < 2; i++) { // for se ejecuta 2 veces porque los argumentos del programa son 2 v:
+    int numero_actual = i ? a : b; // si i vale 0 entonces trabaja con 'a' y si vale 1 con 'b'
     printf("%i %s\n", numero_actual, es_capicua(numero_actual));
   }
   return 0;
