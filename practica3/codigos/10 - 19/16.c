27d943a16f5c3f537558741a9642d34a121aa9ca
Felipeig2 <turipanes234@gmail.com>
2024-07-03 21:35:13 -0300

    Simplifico el ejercicio 16

--- practica3/codigos/10 - 19/16.c
+++ practica3/codigos/10 - 19/16.c
@@ -3,25 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
-  int n = atoi(argv[1]);
-
-  for (int i = 1; i <= n; i++) {
-    printf("%i ", i);
-  }
-  printf("\n");
-  for(int i = 1; i <= n; i++){
-    for(int j = 1; j <= n; j++){
-      if(i == 1){
-        printf("* ");
-      }else if(i == 2){
-        printf("* ");
-      }
-      else{
-        printf(j % i == 0 || i % j == 0 ? "* " : "  ");
-      }
-    }
+int main(int argc, char* argv[]){
+    int n = atoi(argv[1]);
+    for(int i = 1; i <=n; i++){
+        printf("%i ", i);
+    }   
+    printf("\n");
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+            if(j % i == 0 || i % j == 0){
+                printf("* ");
+            }
+            else{
+                printf("  ");
+            }   
+        }
     printf("%i\n", i);
-  }
-
+    }  
 }
