521c7ec13cfed51f169be92bed3969f71b75adef
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-07-04 23:52:14 -0300

    organiza repositorio

--- cifrados/caesar.py
+++
@@ -1,5 +0,0 @@
-abecedario = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-palabra = input("ingrese una palabra\n")
-for caracter in palabra:
-    print(abecedario.find(caracter))
-cifrado = (abecedario.find(caracter))
