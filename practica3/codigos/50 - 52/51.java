65dddc318d530e66afebc024d29203f80c5da9ad
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-07-06 14:36:52 -0300

    termina 51

--- practica3/codigos/50 - 52/51.java
+++ practica3/codigos/50 - 52/51.java
@@ -4,11 +4,15 @@ alguna de esas condiciones no se cumple el programa termina usando la función e
 dibujar usando cuadrados con rect() e ir rotándolos. Investigar en la referencia de Processing las funciones
 rotate() y translate(). El tamaño de la ventana es de 400 × 400 píxeles. */
 size(400, 400);
-int n = 8;
-int x = width / 4;
-int y = height / 4;
+int n = 21;
+int rotacion = 90 / (n/4);
+if(n < 4 && (n % 4 != 0)){
+  exit();
+  print("Hola soy un exit();");
+}
+translate(width/2, height/2);
 for(int i = 4; i <= n; i += 4){
-  rect(x, y, 200, 200);
-  rotate(radians(45));
-  translate(350, -150);
+  fill(0);
+  rect(-104, -104, 208, 208);
+  rotate(radians(rotacion));
 }
