42a9d0fe520a71e8469ec4164b76ce2a6f8f3503
Muzzi Joaquín <155847816+muzzidev@users.noreply.github.com>
2024-08-02 14:54:00 +0000

    agrega 15

---
+++ practica4/10 - 19/15.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int palindromo(char *palabra) {
+  int final = 0;
+  while (palabra[final] != 0) {
+    final++;
+  }
+  int i = 0;
+  while (palabra[i] != 0) {
+    if ((palabra[i]) != (palabra[--final - i])) {
+      return 0;
+      break;
+    }
+    i++;
+  }
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+  char *palabra = argv[1];
+  printf("%i\n", palindromo(palabra));
+  return 0;
+}
