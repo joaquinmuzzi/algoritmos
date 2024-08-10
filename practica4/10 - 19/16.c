21f740f0f2624c57ef6ce18a8a21a2b70ccbeae7
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-08-10 14:49:59 -0300

    agrega 16 mientras git me molesta con el git rebase

--- practica4/10 - 19/16.c
+++ practica4/10 - 19/16.c
@@ -1,25 +1,20 @@
-/* Escribir una función que reciba un argumento de tipo string y busque en un array conocido en tiempo de compilación si el string está en el array.
-Devolver el índice del elemento en el array si fue encontrado. De lo contrario devolver -1. */
+/* Escribir una función que reciba un argumento de tipo string y busque en un array conocido en tiempo de compilación si el string está en el array. Devolver el índice del elemento en el array si fue encontrado. De lo contrario
+devolver -1. */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define N 12
 
-int main(int argc, char *argv[]){
-	char *palabra = argv[1];
-	char *array = "felipe";
-	int indice = 0;
+int index_of(int a, char *array1[], char *word) {
+  for (int i = 0; i < a; i++) {
+    if (strcmp(array1[i], word) == 0) return i+1;
+  }
+  return -1;
+}
 
-	int i = 0;
-	while(palabra[i] != 0){
-		int j = 0;
-		while(array[j] != 0){
-			if(palabra[i] == array[j]){
-				indice = palabra[i];
-			}
-			j++;
-		}
-		i++;
-	}
-	printf("El indice es: %i", indice);
-
+int main(int argc, char *argv[]) {
+  char *array[N] = {"pipe", "milo", "kevin", "felipe", "mateo", "juani", "juan", "porta", "luana", "sofia", "valeria", "cain"};
+  printf("%i\n", index_of(N, array, argv[1]));
+  return 0;
 }
