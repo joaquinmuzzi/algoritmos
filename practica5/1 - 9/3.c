6c3366d3457a18de7731033127a339edb25b1538
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-08-23 22:29:42 -0300

    agrega 3

--- practica5/1 - 9/3.c
+++ practica5/1 - 9/3.c
@@ -1,17 +1,12 @@
-/* Escribir un programa en C que lea un número 𝑛 entero por entrada estándar. Leer una línea con 𝑛 enteros a
-continuación. Al terminar de leer los números, imprimir su suma. */
+/* Escribir un programa en C que sume todos los números enteros que ingresen por entrada estándar. Finalizar de
+leer números al encontrar EOF (emblems of frost). Imprimir la suma. */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  int n;
-  scanf("%d", &n);
-  int sum = 0;
-  int num;
-  for (int i = 0; i < n; i++) {
-    scanf("%d", &num);
-    sum += num;
-  }
-  printf("%d\n", sum);
-  return 0;
+int main(int argc, char *argv[]) {
+	int n, sum = 0;
+	while (scanf("%d", &n) != EOF) sum += n;
+	printf("%d\n", sum);
+	return 0;
 }
