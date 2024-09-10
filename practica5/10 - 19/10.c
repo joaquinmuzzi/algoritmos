1259fe17f50f3031911448b1f86ece062e9813a3
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-09-10 09:05:57 -0300

    termina 10

--- practica5/10 - 19/10.c
+++ practica5/10 - 19/10.c
@@ -1,13 +1,15 @@
 /* Escribir un programa en C que cuente la cantidad de líneas en stdin */
 
 #include <stdio.h>
-#include <stdlib.h>
-int main(void) {
-	char linea;
-	int i = -1;
-	do{
-		scanf("%c", &linea);    
-		i++;
-	} while (linea == '\n');
-	printf("cantidad de saltos de linea: %i", i);
+
+int main() {
+  int c;
+  int line_count = 0;
+  while ((c = getchar()) != EOF) {
+    if (c == '\n') {
+      line_count++;
+    }
+  }
+  printf("Cantidad de saltos de linea: %d\n", line_count);
+  return 0;
 }
