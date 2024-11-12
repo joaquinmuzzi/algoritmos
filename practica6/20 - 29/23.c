7bf7b2d0f707bf2e8ec8aa807262c4c1bd620473
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-11-12 20:57:05 -0300

    agrega 23

---
+++ practica6/20 - 29/23.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int imprimir(int n, int array[n], int i) {
+  if (i == n) return 1;
+  printf("%d", array[i]);
+  return imprimir(n, array, i+1);
+}
+
+int main(int argc, char const *argv[]) {
+  int n = 3;
+  int array[3] = {1, 2, 3};
+  imprimir(n, array, 0);
+  return 0;
+}
