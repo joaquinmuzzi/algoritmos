2648f66b0aa1e8cefb3b2e34aac57e9643d667f4
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-11-12 21:56:10 -0300

    agrega el ejecicio de sofia

--- practica6/20 - 29/25.c
+++ practica6/20 - 29/25.c
@@ -1,18 +1,20 @@
+/* Escribir una función recursiva en C que devuelva el promedio de los elementos en un array de enteros. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int promedio(int i, int array[], int n, int total) {
-    if (i == total) { 
-        return n / total; 
-    }
-    return promedio(i + 1, array, n + array[i], total); 
+  if (i == total) { 
+    return n / total; 
+  }
+  return promedio(i + 1, array, n + array[i], total); 
 }
 
 int main(int argc, char *argv[]) {
-    int i = 0;  
-    int n = 0;  
-    int total = 3; 
-    int array[] = {1, 2, 3};  
-    printf("%d\n", promedio(i, array, n, total)); 
-    return 0;
+  int i = 0;  
+  int n = 0;  
+  int total = 3; 
+  int array[] = {1, 2, 3};  
+  printf("%d\n", promedio(i, array, n, total)); 
+  return 0;
 }
